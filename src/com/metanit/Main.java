66f5a6320db3ac8c82d5a0e2cfd4ae443853aeea
alexxten <alexxten13@gmail.com>
2019-11-10 14:55:29 +0300

    add input from console

--- src/com/metanit/Main.java
+++ src/com/metanit/Main.java
@@ -1,9 +1,17 @@
 package com.metanit;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code
         System.out.println("Hello world");
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        System.out.println(num);
+        scan.close();
+
+        char s_num = (char) num;
+        System.out.println(s_num);
     }
 }

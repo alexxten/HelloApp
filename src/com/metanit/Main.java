a4d947aecf639fbb5ad22cdb32e10c15ca802d6b
alexxten <alexxten13@gmail.com>
2019-11-10 19:19:08 +0300

    add functions

--- src/com/metanit/Main.java
+++ src/com/metanit/Main.java
@@ -13,5 +13,23 @@ public class Main {
 
         char s_num = (char) num;
         System.out.println(s_num);
+
+
+        System.out.println(sum(2, 3));          // 5
+        System.out.println(sum(4.5, 3.2));      // 7.7
+//        System.out.println(sum(4, 3, 7));       // 14
     }
+
+    static int sum(int x, int y){
+
+        return x + y;
+    }
+    static double sum(double x, double y){
+
+        return x + y;
+    }
+//    static int sum(int x, int y, int z){
+//
+//        return x + y + z;
+//    }
 }
